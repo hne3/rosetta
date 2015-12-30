a9dbf8f54ca39a17418aa5f87f6a3f60fa6aa869
Philip Guo <philip@pgbovine.net>
2015-12-29 20:42:50 -0500

    bajj

--- src/relement.js
+++ src/relement.js
@@ -1,12 +1,5 @@
 // Rosetta classes start with 'R' so as not to conflict with built-in types
 
-// TODOs:
-// - maybe render as a simple <td></td> instead of a full table so that
-//   it's easily embeddable in other elements; however if the 'hanSolo' prop
-//   is enabled, then render this as a solo element with a surrounding
-//   <table></table>. this way, there aren't too many unnecessary levels
-//   of table nesting when placed in a RCollection
-
 import React from 'react';
 
 export class RElement extends React.Component {
